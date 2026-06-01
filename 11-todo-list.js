2b53c3d26d78403f8aff9f552040238fa4c63c13
swopnilup <swopnil45@gmail.com>
2026-06-01 17:28:34 +0545

    add foreach loop instead of for

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -1,4 +1,4 @@
-const todoList =[{
+const todoList = [{
   name: 'make dinner',
   dueDate: '2022-12-22'
 },
@@ -11,46 +11,34 @@ renderTodoList();
 function renderTodoList(){
     let todoListHTML = '';
 
-    for(i = 0; i < todoList.length; i++){
-      const todoObject = todoList[i];
-      //const name = todoObject.name;
-      //const dueDate =todoObject.dueDate;
-      const {name, dueDate} =todoObject;
+    todoList.forEach(function(todoObject, index){
+      const {name, dueDate} = todoObject;
 
-      const html =`
+      const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
       <button onclick="
-        todoList.splice(${i}, 1);
+        todoList.splice(${index}, 1);
         renderTodoList();     
       " class="delete-button">Delete</button>  
       `;
       todoListHTML += html;
-    }
-    console.log(todoListHTML);
-
+    });
+    
     document.querySelector('.js-todo-list')
-    .innerHTML = todoListHTML;
-}
+      .innerHTML = todoListHTML;
+}                             // ← Add this closing brace
+
 function addTodo(){
- const inputElement= document.querySelector('.js-name-input');
- const name = inputElement.value;
- 
- 
- //console.log(name);
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
   const dateInputElement = document.querySelector('.js-due-date-input');
   const dueDate = dateInputElement.value;
- 
- 
-  todoList.push({
-   // name: name,
-    //dueDate: dueDate,
-    name,
-    dueDate
-    });
- console.log(todoList)
 
- inputElement.value = '';
-
- renderTodoList();
+  todoList.push({ name, dueDate });
+  
+  console.log(todoList);
+  inputElement.value = '';
+  renderTodoList();
 }
